c1cb8e389ec96623620785859c23275513a2e8ae
Ishan Shanware <Ishan.Shanware@iiitb.org>
2020-05-05 19:45:28 +0530

    projects left

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,29 +1,33 @@
+// Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
-//const menuBrand = document.querySelector('.menu-brand');
 const menuNav = document.querySelector('.menu-nav');
-const NavItems = document.querySelectorAll('.nav-item');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
 
-
-
-let showMenu =  false;
+// Set Initial State Of Menu
+let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-    if(!showMenu){
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-       // menuBrand.classList.add('show');
-        NavItems.forEach(item => item.classList.add('show'));
-        showMenu = true;
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
 
-    }
-    else{
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-       // menuBrand.classList.remove('show');
-        NavItems.forEach(item => item.classList.remove('show'));
-        showMenu = false;
-    }
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    // Set Menu State
+    showMenu = false;
+  }
 }
